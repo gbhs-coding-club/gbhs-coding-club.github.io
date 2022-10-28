3a56a71fb11d43c49e783bdc81d9b15f146fd2b8
Paradoxian <73145304+ProgrammingParadox@users.noreply.github.com>
2022-10-28 01:06:06 +0000

    make lexer

--- people/ProgrammingParadox/scripts/main.js
+++ people/ProgrammingParadox/scripts/main.js
@@ -10,10 +10,56 @@ let line = function(value, filter = true, prepend = true){
   lines.append(line);
 };
 
+let Lexer = function(string){
+  this.text = string;
+  this.index = 0;
+
+  this.tokens = [];
+};
+Lexer.prototype.peek = function(i=1){
+  return this.text.charAt(this.index + i);
+};
+Lexer.prototype.eatWhitespace = function(){
+  while(this.index < this.text.length && this.text[this.index].match(/[\s\n]/)){
+    this.index ++;
+  }  
+};
+Lexer.prototype.eat = function(){
+  let token = {
+    value: "",
+  };
+
+  let cur = this.text[this.index];
+
+  while((cur = this.text.charAt(this.index)).match(/[^\s\n]/)){
+    token.value += this.text.charAt(this.index);
+
+    console.log(this.index, token.value);
+    
+    this.index ++;
+  }
+
+  this.eatWhitespace();
+
+  return token;
+};
+
+let l = new Lexer("this is a test123 ++ -- + - +-");
+console.log(l.eat());
+console.log(l.eat());
+console.log(l.eat());
+console.log(l.eat());
+console.log(l.eat());
+console.log(l.eat());
+console.log(l.eat());
+console.log(l.eat());
+console.log(l.eat());
+
+// uhh
 let parse = function(text = ""){
-    if(text == "help"){
-      line("<b>help</b> doesn't do anything yet. Sorry. I lied.", false, false);
-    }
+  if(text == "help"){
+    line("<b>help</b> doesn't do anything yet. Sorry. I lied.", false, false);
+  }
 };
 
 let typing = document.getElementById('typing');
