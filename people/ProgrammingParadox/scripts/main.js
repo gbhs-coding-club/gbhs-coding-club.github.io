f91af6ecddcf64d436df59c85847b55590fe0a95
Paradoxian <73145304+ProgrammingParadox@users.noreply.github.com>
2022-10-28 00:40:32 +0000

    Updating my profile :/

--- people/ProgrammingParadox/scripts/main.js
+++ people/ProgrammingParadox/scripts/main.js
@@ -1,19 +1,25 @@
 
+// eventually put in class or smth
+let line = function(value, filter = true, prepend = true){
+  let line = document.createElement("div");
+  line.classList = "line" + (prepend ? " prepend" : "");
+
+  filter ? line.textContent = value : line.innerHTML = value;
+
+  let lines = document.getElementsByClassName("lines")[0];
+  lines.append(line);
+};
+
 let parse = function(text = ""){
-    
+    if(text == "help"){
+      line("<b>help</b> doesn't do anything yet. Sorry. I lied.", false, false);
+    }
 };
 
 let typing = document.getElementById('typing');
 typing.addEventListener("keydown", (event) => {
   if(event.key == "Enter"){
-    let line = document.createElement("div");
-    line.classList = "line";
-
-    line.textContent = event.target.value;
-
-    let lines = document.getElementsByClassName("lines")[0];
-    lines.append(line);
-
+    line(event.target.value);    
     parse(event.target.value);
 
     event.target.value = "";
