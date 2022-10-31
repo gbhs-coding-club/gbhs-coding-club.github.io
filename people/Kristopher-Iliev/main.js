9c560f395ef1034f55df2335c48fb7fefcef5912
Paradoxian <73145304+ProgrammingParadox@users.noreply.github.com>
2022-10-31 17:01:29 +0000

    Push Kristopher's changes

--- people/Kristopher-Iliev/main.js
+++ people/Kristopher-Iliev/main.js
@@ -1,5 +1,5 @@
-console.log("Hello, World!"); // This thing starts the thingy
-
+console.log("Get outta here!"); // This Starts the Console
+// Scans constant for letter numbers, then prints them
 const name = "Kris";
 for(let i = 0; i < name.length; i++){
   let e = document.createElement("div");
