ea8cc38445d41f4b307e6517a80ac329d68a34c8
Kristopher Iliev <115750717+Kristopher-Iliev@users.noreply.github.com>
2022-12-02 00:05:21 +0000

    Background Color and Removal of Java File.

---
+++ people/ProgrammingParadox/scripts/modules/definition.js
@@ -0,0 +1,20 @@
+
+/*
+
+// https://stackoverflow.com/questions/9725675/is-there-a-standard-format-for-command-line-shell-help-text
+
+export help = definition(
+  "help", [
+    [
+      "",
+      (def) => {
+        line(def.description, false, false);
+      }
+    ]
+  ]
+);
+
+*/
+let definition = function(name){
+  
+};
