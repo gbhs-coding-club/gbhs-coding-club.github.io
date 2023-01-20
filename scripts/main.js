6048560b5605fe5d68c9cb14bda700e861eb0b7a
Paradoxian <73145304+ProgrammingParadox@users.noreply.github.com>
2023-01-20 23:06:42 +0000

    Synced people on home page with their github profiles

--- scripts/main.js
+++ scripts/main.js
@@ -49,23 +49,44 @@ Promise.all(Object.keys(names).map(a => fetch(`https://api.github.com/users/${a}
       i<entries.length;
       i++, person=entries[i]
     ) {
+      // main data
+      let profile = data[i];
+
+      console.log(person, profile);
+      
       // main parent element
       let nameElement = document.createElement("div");
       nameElement.classList.add("name");
+
+      // Github profile picture
+      let imageElement = document.createElement("img");
+      imageElement.classList.add("name-image");
+      imageElement.setAttribute("src", profile.avatar_url);
+      imageElement.setAttribute("width", "50");
+      imageElement.setAttribute("height", "50");
+
+      nameElement.appendChild(imageElement);
+
+      // stat holder element
+      let statElement = document.createElement("div");
+      statElement.classList.add("name-stats");
 
       // Link to their page
       let linkElement = document.createElement("a");
       linkElement.setAttribute("href", "/people/" + person[0]);
       linkElement.textContent = person[1];
-      linkElement.classList.add("name-title");
+      linkElement.classList.add("name-link");
     
-      nameElement.appendChild(linkElement);
+      statElement.appendChild(linkElement);
 
-      // Github profile picture
-      let imageElement = document.createElement("img");
-      imageElement.classList.add("name-image");
-      // imageElement.setAttribute("src");
-        
+      // github username
+      let usernameElement = document.createElement("div");
+      usernameElement.classList.add("name-username");
+      usernameElement.textContent = profile.login ?? person[0];
+
+      statElement.appendChild(usernameElement);
+
+      nameElement.appendChild(statElement);
       namesElement.appendChild(nameElement);
     }
   })
